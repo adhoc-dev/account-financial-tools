1dc87b72005bfd8a5f061097edab5955c138820e
Camila Vives <cav@adhoc.com.ar>
2026-01-19 18:05:08 +0000

    [IMP] mail_compose_message: mark payment as sent from email wizard When sending payment emails using the mail compose message wizard, it is useful to mark the payment as sent automatically once the email is sent. This commit introduces a feature that allows users to mark the payment as sent directly from the email wizard, streamlining the workflow and reducing manual steps.

---
+++ account_ux/wizards/mail_compose_message.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+
+class MailComposer(models.TransientModel):
+    _inherit = "mail.compose.message"
+
+    def _action_send_mail(self, auto_commit=False):
+        super()._action_send_mail(auto_commit=auto_commit)
+
+        for wizard in self:
+            res_ids = wizard._evaluate_res_ids()
+
+            if wizard.model == "account.payment":
+                self.env["account.payment"].browse(res_ids).write({"is_sent": True})
